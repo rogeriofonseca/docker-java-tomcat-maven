12eb0588b83a1bf724c7f9652af20a3c6a44929d
Rogerio Fonseca <rogerio.fonseca@gmail.com>
2017-03-24 22:39:58 -0300

    Configurando projeto para em containers Docker

--- src/main/java/br/com/caelum/JpaConfigurator.java
+++ src/main/java/br/com/caelum/JpaConfigurator.java
@@ -1,5 +1,7 @@
 package br.com.caelum;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import java.beans.PropertyVetoException;
 import java.util.Properties;
 
 import javax.persistence.EntityManagerFactory;
@@ -7,7 +9,6 @@ import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
@@ -17,44 +18,47 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 public class JpaConfigurator {
 
-	@Bean
-	public DataSource getDataSource() {
-	    DriverManagerDataSource dataSource = new DriverManagerDataSource();
+    @Bean
+    public DataSource getDataSource() throws PropertyVetoException {
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        dataSource.setDriverClass("com.mysql.jdbc.Driver");
+        dataSource.setJdbcUrl("jdbc:mysql://192.168.99.100/projeto_jpa");
+        dataSource.setUser("root");
+        dataSource.setPassword("root");
 
-	    dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-	    dataSource.setUrl("jdbc:mysql://127.0.0.1/projeto_jpa");
-	    dataSource.setUsername("root");
-	    dataSource.setPassword("root");
+        dataSource.setMinPoolSize(2);
+        dataSource.setMaxPoolSize(5);
+        dataSource.setNumHelperThreads(5);
 
-	    return dataSource;
-	}
+        return dataSource;
+    }
 
-	@Bean
-	public LocalContainerEntityManagerFactoryBean getEntityManagerFactory(DataSource dataSource) {
-		LocalContainerEntityManagerFactoryBean entityManagerFactory = new LocalContainerEntityManagerFactoryBean();
+    @Bean
+    public LocalContainerEntityManagerFactoryBean getEntityManagerFactory(DataSource dataSource) {
+        LocalContainerEntityManagerFactoryBean entityManagerFactory = new LocalContainerEntityManagerFactoryBean();
 
-		entityManagerFactory.setPackagesToScan("br.com.caelum");
-		entityManagerFactory.setDataSource(dataSource);
+        entityManagerFactory.setPackagesToScan("br.com.caelum");
+        entityManagerFactory.setDataSource(dataSource);
 
-		entityManagerFactory
-				.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        entityManagerFactory
+                .setJpaVendorAdapter(new HibernateJpaVendorAdapter());
 
-		Properties props = new Properties();
+        Properties props = new Properties();
 
-		props.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5InnoDBDialect");
-		props.setProperty("hibernate.show_sql", "true");
-		props.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+        props.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5InnoDBDialect");
+        props.setProperty("hibernate.show_sql", "true");
+        props.setProperty("hibernate.hbm2ddl.auto", "create-drop");
 
-		entityManagerFactory.setJpaProperties(props);
-		return entityManagerFactory;
-	}
+        entityManagerFactory.setJpaProperties(props);
+        return entityManagerFactory;
+    }
 
-	@Bean
-	public JpaTransactionManager getTransactionManager(EntityManagerFactory emf) {
-		JpaTransactionManager transactionManager = new JpaTransactionManager();
-		transactionManager.setEntityManagerFactory(emf);
+    @Bean
+    public JpaTransactionManager getTransactionManager(EntityManagerFactory emf) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(emf);
 
-		return transactionManager;
-	}
+        return transactionManager;
+    }
 
 }
